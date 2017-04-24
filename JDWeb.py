24dc582e4dfc0b63121ba5ce1a43067d795d6c2a
lon91ong <lon91ong@gmail.com>
2017-04-24 10:17:51 +0800

    Update JDWeb.py

--- JDWeb.py
+++ JDWeb.py
@@ -14,13 +14,50 @@ import os
 import time
 import json
 import random
-
+import ctypes
 import sys
 #imp.reload(sys)
 #sys.setdefaultencoding('utf-8')
 # get function name
 FuncName = lambda n=0: sys._getframe(n + 1).f_code.co_name
+                                    
+SetSystemTime = ctypes.windll.kernel32.SetSystemTime
+GetSystemTime = ctypes.windll.kernel32.GetSystemTime
 
+class SYSTEMTIME(ctypes.Structure):
+    c_ushort= ctypes.c_ushort
+    _fields_ =  (
+                ('wYear', c_ushort), 
+                ('wMonth', c_ushort), 
+                ('wDayOfWeek', c_ushort), 
+                ('wDay', c_ushort), 
+                ('wHour', c_ushort), 
+                ('wMinute', c_ushort), 
+                ('wSecond', c_ushort), 
+                ('wMilliseconds', c_ushort), 
+                )
+    def __str__(self):
+        return '%4d%02d%02d%02d%02d%02d.%03d' % (self.wYear,self.wMonth,self.wDay,self.wHour,self.wMinute,self.wSecond,self.wMilliseconds)
+
+def updateSystemTime():
+    url= 'http://www.jd.com'
+    try:
+        response= requests.get(url)
+        date= response.headers['date']
+        gmt=time.strptime(date[5:25], "%d %b %Y %H:%M:%S")
+        st=SYSTEMTIME(gmt.tm_year,gmt.tm_mon,gmt.tm_wday,gmt.tm_mday,gmt.tm_hour,gmt.tm_min,gmt.tm_sec,0)
+        SetSystemTime(ctypes.byref(st))
+        print('网络校时成功！')
+    except Exception as ex:
+        print(ex)
+        print('网络校时失败！')
+        return False
+    return True
+
+def printCurTime():
+    now= time.localtime(time.time())
+    print('当前系统时间:', time.strftime("%Y-%m-%d %H:%M:%S", now))
+    
 def tags_val(tag, key='', index=0):
     '''
     return html tag list attribute @key @index
@@ -49,7 +86,6 @@ def tag_val(tag, key=''):
     else:
         txt = tag.text
         return txt.strip(' \t\r\n') if txt else ''
-
 
 class JDWrapper(object):
     '''
